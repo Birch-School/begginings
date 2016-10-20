9499dc20e32205ae952a001e02f91bfbbbaed911
Harry Sweet <hsweet52@gmail.com>
2016-10-20 08:09:39 -0400

    Add files via upload
    Try this in Idle to learn how to use some of the most basic tools in programming, variables and loops

---
+++ graphie.py
@@ -0,0 +1,10 @@
+# Try this code in Idle, change some of the numbers and see what happens
+
+#counter 
+counter = 1 
+
+# How many times do you think the loop will repeat?
+while (counter < 60):
+    print ("*" * counter, end = " \\ ") #fancy print 
+    counter  = counter + 1
+    print (counter)
